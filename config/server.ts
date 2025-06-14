56da516d3ecaa29ea99bd06f09555c2a76b6f6c6
UnaCuentaFalsaMas <garahxp@gmail.com>
2025-06-13 23:23:51 -0400

    a este punto solo me hace mas fuerte

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   host: process.env.HOST || '0.0.0.0',
-  port: process.env.PORT || 1337,
+  port: process.env.PORT || 10000,
   app: {
     keys: process.env.APP_KEYS.split(','),
   },
