8fbcf64976787454892cad33b40485fc985d64f8
UnaCuentaFalsaMas <garahxp@gmail.com>
2025-06-13 23:04:01 -0400

    la fe es lo ultimo que se pierde

--- config/database.ts
+++ config/database.ts
@@ -1,11 +1,13 @@
 import path from 'path';
 import {parse} from 'pg-connection-string';
 
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  const conf = parse(env("DATABASE_URL"));
+  return {
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: conf.host,
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
@@ -18,7 +20,7 @@ module.exports = ({ env }) => ({
     },
     debug: false,
   },
-});
+}};
 // export default ({ env }) => {
 //   const client = env('DATABASE_CLIENT', 'postgres');
 //   const config = parse(env('DATABASE_URL'));
